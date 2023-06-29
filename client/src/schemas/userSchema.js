15553cc445a6f33559ca07f74966527cf1c7500d
fdimopoulos <fdimopoulos@hotmail.com>
2023-06-29 15:41:45 +0300

    fix error messages on schema

--- client/src/schemas/userSchema.js
+++ client/src/schemas/userSchema.js
@@ -6,18 +6,18 @@ const passwordRules = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{5,20}$/;
 export const userSchema = Yup.object().shape({
     firstName: Yup.string()
         .min(3, "First Name is too short")
-        .max(30, "First Name must be at most 30 characters")
+        .max(30, "First Name must be no more than 30 characters")
         .required("Required"),
     lastName: Yup.string()
         .min(3, "Last Name is too short")
-        .max(30, "Last Name must be at most 30 characters")
+        .max(30, "Last Name must be no more than 30 characters")
         .required("Required"),
     email: Yup.string()
         .email("Please enter a valid email address")
         .required("Required"),
     password: Yup.string()
         .min(5, "Password is too short")
-        .max(20, "Password must be at most 20 characters")
+        .max(20, "Password must be no more than 20 characters")
         .matches(passwordRules, {
             message: "Please create a stronger password",
         })
